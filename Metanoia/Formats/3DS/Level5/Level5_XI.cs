3b049102e39e31a658d1425297cd7842897efd8d
ploaj_the_otter <ploaj_the_otter@yahoo.com>
2019-11-18 13:07:18 -0600

    Added non-3ds RGB8 to XI

--- Metanoia/Formats/3DS/Level5/Level5_XI.cs
+++ Metanoia/Formats/3DS/Level5/Level5_XI.cs
@@ -20,17 +20,28 @@ namespace Metanoia.Formats._3DS.Level5
 
         public byte[] ImageData { get; set; }
 
+        private bool SwitchFile { get; set; } = false;
+
         public static GenericTexture ToGenericTexture(byte[] xifile)
         {
             GenericTexture tex = new GenericTexture();
             Level5_XI xi = new Level5_XI();
             xi.Open(xifile);
-            if (xi.ImageFormat == 0x1D)
+            if (xi.SwitchFile && xi.ImageFormat == 0x1D)
             {
                 tex.Mipmaps.Add(xi.BuildImageDataFromBlock(8)[0]);
                 tex.InternalFormat = OpenTK.Graphics.OpenGL.PixelInternalFormat.CompressedRgbS3tcDxt1Ext;
                 tex.Width = (uint)xi.Height;
                 tex.Height = (uint)xi.Width; 
+            }
+            else
+            if (xi.SwitchFile && xi.ImageFormat == 0x1)
+            {
+                tex.Mipmaps.Add(xi.BuildImageData()[0]);
+                tex.InternalFormat = OpenTK.Graphics.OpenGL.PixelInternalFormat.Rgb8;
+                tex.PixelFormat = OpenTK.Graphics.OpenGL.PixelFormat.Rgb;
+                tex.Width = (uint)xi.Height;
+                tex.Height = (uint)xi.Width;
             }
             else
             {
@@ -68,13 +79,16 @@ namespace Metanoia.Formats._3DS.Level5
                 int imageDataOffset = someTable + someTableSize;
 
                 byte[] _tileData = Decompress.Level5Decom(r.GetSection((uint)someTable, someTableSize));
-                
+
+                if (_tileData.Length > 2 && _tileData[0] == 0x53 && _tileData[1] == 0x04)
+                    SwitchFile = true;
+
                 using (DataReader tileData = new DataReader(new MemoryStream(_tileData)))
                 {
                     int tileCount = 0;
                     while (tileData.Position + 2 <= tileData.BaseStream.Length)
                     {
-                        int i = type == 0x1D ? tileData.ReadInt32() : tileData.ReadInt16();
+                        int i = SwitchFile ? tileData.ReadInt32() : tileData.ReadInt16();
                         if (i > tileCount) tileCount = i;
                         Tiles.Add(i);
                     }
@@ -115,6 +129,52 @@ namespace Metanoia.Formats._3DS.Level5
         /// </summary>
         /// <param name="blockSize"></param>
         /// <returns></returns>
+        private List<byte[]> BuildImageData()
+        {
+            List<byte[]> pixels = new List<byte[]>();
+
+            var mip1 = new byte[Width * Height * 3];
+
+            var tileSize = 64;
+            var bpp = 3;
+
+            var x = 0;
+            var y = 0;
+
+            for (int i = 2; i < Tiles.Count; i++)
+            {
+                int code = Tiles[i];
+
+                // only need the first mip for now really...
+                if ((i - 2) * tileSize * bpp >= mip1.Length)
+                    break;
+
+                for (int h = 0; h < tileSize; h++)
+                {
+                    var x1 = h / 8;
+                    var y1 = h % 8;
+                    for(int j = 0; j < bpp; j++)
+                        mip1[((x + x1) * Height + (y + y1)) * 3 + j] = ImageData[code * (tileSize * bpp) + h * bpp + j];
+                }
+                y += 8;
+
+                if (y >= Height)
+                {
+                    y = 0;
+                    x += 8;
+                }
+            }
+
+            pixels.Add(mip1);
+
+            return pixels;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="blockSize"></param>
+        /// <returns></returns>
         private List<byte[]> BuildImageDataFromBlock(int blockSize)
         {
             List<byte[]> pixels = new List <byte[]>();
@@ -138,10 +198,14 @@ namespace Metanoia.Formats._3DS.Level5
             return pixels;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public Bitmap ToBitmap()
         {
             int he = (int)Math.Ceiling(((Tiles.Count) * 8f) / 128);
-            
+
             Bitmap tileSheet = _3DSImageTools.DecodeImage(ImageData, Tiles.Count * 8, 8, (_3DSImageTools.Tex_Formats)ImageFormat);
             var img = new Bitmap(Width, Height, PixelFormat.Format32bppArgb);
 
---
+++ Metanoia/Tools/ImageTools.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metanoia.Tools
+{
+    public class ImageTools
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public static Bitmap RGB8ToBitmap(byte[] data, int width, int height)
+        {
+            int[] pixels = new int[width * height];
+
+            var img = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+
+            for(int i = 0; i < pixels.Length; i++)
+            {
+                if (i * 3 + 3 > data.Length)
+                    break;
+                pixels[0] = ((255 & 0xFF) << 24) | ((data[i * 3] & 0xFF) << 24) | ((data[i * 3 + 1] & 0xFF) << 24) | ((data[i * 3 + 2] & 0xFF));
+            }
+
+            var bmpData = img.LockBits(new Rectangle(0, 0, img.Width, img.Height), ImageLockMode.WriteOnly, img.PixelFormat);
+            Marshal.Copy(pixels, 0, bmpData.Scan0, pixels.Length);
+            img.UnlockBits(bmpData);
+
+            return img;
+        }
+
+    }
+}
